e77a4d93d52b4adc07abdc4cdbfd31c93cad89ca
alphad1 <17600147295@163.com>
2018-05-28 16:21:23 +0800

    pang bin feng

--- mall-parent/mall-manager/mall-manager-service/src/main/java/com/alpha/mall/manager/service/impl/BaseFileGenertorServiceImpl.java
+++ mall-parent/mall-manager/mall-manager-service/src/main/java/com/alpha/mall/manager/service/impl/BaseFileGenertorServiceImpl.java
@@ -26,6 +26,7 @@ public class BaseFileGenertorServiceImpl implements BaseFileGenertorService {
 
     public static void main(String[] args) {
         System.out.println("1212");
+        System.out.println("sadfsafa");
     }
 
     @Autowired
